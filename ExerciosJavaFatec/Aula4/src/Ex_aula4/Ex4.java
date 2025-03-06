068d43eedc151ea51f1980681825d0f3facc055c
Ryan Lucena Silva <134936752+Ryanlssv@users.noreply.github.com>
2025-03-06 11:22:42 -0300

    add

---
+++ ExerciosJavaFatec/Aula4/src/Ex_aula4/Ex4.java
@@ -0,0 +1,17 @@
+package Ex_aula4;
+import java.util.Scanner;
+public class Ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		int n=7 ; 
+		double num=7;
+		System.out.printf("\n Divisão de inteiros: %d ", n/2);
+		System.out.printf("\n resto da Divisão de inteiros: %d ", n%2);
+		System.out.printf("\n Divisão de reais: %f ",num/2);
+	}
+
+}
+
+
